df3388c8200f9e1907bba4203a9ed730876559ff
Blue <tonyone0902@gmail.com>
2014-02-08 20:55:01 +0800

    update generatefile name

--- index.js
+++ index.js
@@ -61,14 +61,14 @@ module.exports = function ($youmeb, $injector, $config, $generator, $prompt) {
       });
       gn_app.createFile('./app.ls', './app.ls', {}, done);
       gn_bower.createFile('./bower.json', './bower.json', {
-        name: result.name[0].toUpperCase() + result.name.substr(1)
+        name: result.Projectname[0].toUpperCase() + result.Projectname.substr(1)
       }, done);
       gn_gulpfile.createFile('./gulpfile.ls', './gulpfile.ls', {}, done);
       gn_package.createFile('package.json', './package.json', {
-        name: result.name[0].toUpperCase() + result.name.substr(1)
+        name: result.Projectname[0].toUpperCase() + result.Projectname.substr(1)
       }, done);
       gn_app_index.createFile('./app/index.html', './app/index.html', {
-        name: result.name[0].toUpperCase() + result.name.substr(1)
+        name: result.Projectname[0].toUpperCase() + result.Projectname.substr(1)
       }, done);
 
     });
